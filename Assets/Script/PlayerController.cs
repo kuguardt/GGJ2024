0ef2266d7783cb449d7faa4bf0df3317a1d358c1
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2024-01-26 23:51:46 -0800

    Add Aniamtion

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,8 +9,6 @@ public class PlayerController : MonoBehaviour
 
     private int playerId;
     
-    public float speed = 10.0f;
-
     private Vector2 movementInput;
     
     [SerializeField] List<Color> playerColors = new List<Color>(){Color.red, Color.blue, Color.green, Color.yellow};
@@ -22,16 +20,117 @@ public class PlayerController : MonoBehaviour
         playerCount++;
         GetComponent<SpriteRenderer>().color = playerColors[playerId];
     }
+    
+    
+    private float horizontal;
+    [SerializeField] float originSpeed;
+    [SerializeField] float currentSpeed;
+    private bool isFacingRight = true;
+
+    private bool isJumping;
+    bool extraJump;
+    [SerializeField] float extraJumpPower;
+
+    [SerializeField] float jumpPower;
+    [SerializeField] float jumpBufferTime;
+    [SerializeField] float jumpCD;
+    private float jumpBufferCount;
+
+    [SerializeField] float coyoteTime;
+    private float coyoteTimeCount;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpeed = originSpeed;
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(new Vector3(movementInput.x,movementInput.y,0) * (speed * Time.deltaTime));
-        
-    }
     
     public void OnMove(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = movementInput.x;
+
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            extraJump = false;
+        }
+
+        if (IsGrounded())
+        {
+            coyoteTimeCount = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCount -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCount = jumpBufferTime;
+            if (IsGrounded() || extraJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, extraJump ? extraJumpPower : jumpPower);
+                extraJump = !extraJump;
+            }
+        }
+        else
+        {
+            jumpBufferCount -= Time.deltaTime;
+        }
+
+        if (coyoteTimeCount > 0f && jumpBufferCount > 0f && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+
+            jumpBufferCount = 0f;
+
+            StartCoroutine(JumpCooldown());
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyoteTimeCount = 0f;
+        }
+
+        //Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * currentSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    // private void Flip()
+    // {
+    //     if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+    //     {
+    //         isFacingRight = !isFacingRight;
+    //         transform.Rotate(0f, 180f, 0f);
+    //     }
+    // }
+
+    private IEnumerator JumpCooldown()
+    {
+        isJumping = true;
+        yield return new WaitForSeconds(jumpCD);
+        isJumping = false;
     }
 }
