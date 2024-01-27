4432ae9656a23b49beed3bfd15c1fc6703668346
ArkuDada <39034809+ArkuDada@users.noreply.github.com>
2024-01-27 02:47:23 -0800

    GASSS GAS GASSSSS

--- Assets/Script/PlayerSkill.cs
+++ Assets/Script/PlayerSkill.cs
@@ -1,14 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Script
 {
     public class PlayerSkill : MonoBehaviour
     {
+        [SerializeField] private GameObject gasPrefab;
+        [SerializeField] private float SkillCoolDown = 1f;
+        
+        private bool canUseSkill = true;
         public void OnSkill(InputAction.CallbackContext context)
         {
             Debug.Log(
                 $"{context.action.name} performed: {context.performed} started: {context.started} canceled: {context.canceled}");
+            if (canUseSkill)
+            {
+                StartCoroutine(Gas());
+            }
+        }
+
+        private IEnumerator Gas()
+        {
+            canUseSkill = false;
+            Instantiate(gasPrefab, transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(SkillCoolDown);
+            canUseSkill = true;
         }
     }
 }
