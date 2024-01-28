9eeea6433146088779d4d31cd437f5f67b6a2e8c
Kuguardt <kuguardt@gmail.com>
2024-01-28 15:02:09 -0800

    outlining

--- Assets/Script/Toilet.cs
+++ Assets/Script/Toilet.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class Toilet : MonoBehaviour
 {
+    public Outline toiletoutline;
+
     public bool isOccupied = false;
 
     private void OnEnable()
     {
         isOccupied = false;
+    }
+
+    private void Update()
+    {
+        if (isOccupied == true)
+        {
+            toiletoutline.enabled = false;
+        }
+        else
+        {
+            toiletoutline.enabled = true;
+        }
     }
 }
