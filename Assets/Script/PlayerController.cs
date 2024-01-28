e8dad2a51ebf0222f3e187ec9eeb5447a57f5193
Kuguardt <kuguardt@gmail.com>
2024-01-27 20:24:57 -0800

    menu with configuration players

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
 
     private PlayerInputMap controls;
 
+    private bool inConfigRoom = false;
+
     [ColorUsageAttribute(true, true, 0f, 8f, 0.125f, 3f)] [SerializeField]
     List<Color> playerColors = new List<Color>() { Color.red, Color.blue, Color.green, Color.yellow };
 
@@ -52,6 +54,11 @@ public class PlayerController : MonoBehaviour
     {
         if (obj.action.name == controls.Gameplay.Interact.name)
         {
+            if (inConfigRoom)
+            {
+
+                return;
+            }
             OnInteract(obj);
         }
         if (obj.action.name == controls.Gameplay.Skill.name)
@@ -91,7 +98,12 @@ public class PlayerController : MonoBehaviour
         playerConfig = config;
         playerId = playerConfig.PlayerIndex;
         GetComponent<SpriteRenderer>().material.color = playerColors[playerId];
-        playerConfig.Input.onActionTriggered += Input_onActionTriggered;
+        config.Input.onActionTriggered += Input_onActionTriggered;
+    }
+
+    public void SetInConfigurationRoom(bool fact)
+    {
+        inConfigRoom = fact;
     }
 
     public void OnMove(CallbackContext context)
